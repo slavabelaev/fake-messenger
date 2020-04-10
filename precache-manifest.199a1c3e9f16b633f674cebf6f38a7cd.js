5caba41a721fd3fb8417f666d7f91187d47d29be
Slava Belaev <slav@belaev.pro>
2020-04-10 13:08:20 +0300

    Updates

---
+++ precache-manifest.199a1c3e9f16b633f674cebf6f38a7cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26a426ef1526d39c1c8ab758b1d3f2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04add8a855ee9d5ea3b7",
+    "url": "/static/js/2.51bc0205.chunk.js"
+  },
+  {
+    "revision": "1f9fe3f702b325dd72313f3618c1f0d1",
+    "url": "/static/js/2.51bc0205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e5ef930c9c01db83436",
+    "url": "/static/js/main.739e511d.chunk.js"
+  },
+  {
+    "revision": "c7484ddd9849a3981c08",
+    "url": "/static/js/runtime-main.537c79f7.js"
+  }
+]);
