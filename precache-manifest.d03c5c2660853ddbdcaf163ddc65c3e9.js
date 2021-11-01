11deb3b14e410c89c9b8154ce98f445c0545910d
Slava Belaev <slav@belaev.pro>
2021-11-01 23:05:17 +0300

    Updates

---
+++ precache-manifest.d03c5c2660853ddbdcaf163ddc65c3e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4352c5e27a0a0d3af2ebfefc3bdf96",
+    "url": "/fake-messenger/index.html"
+  },
+  {
+    "revision": "719b70e0afa998125799",
+    "url": "/fake-messenger/static/js/2.3f6b212e.chunk.js"
+  },
+  {
+    "revision": "1f9fe3f702b325dd72313f3618c1f0d1",
+    "url": "/fake-messenger/static/js/2.3f6b212e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8bcffca8a23cbaf7043",
+    "url": "/fake-messenger/static/js/main.2fd176c1.chunk.js"
+  },
+  {
+    "revision": "91b0cf7ad4e59d053f84",
+    "url": "/fake-messenger/static/js/runtime-main.01fde139.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.199a1c3e9f16b633f674cebf6f38a7cd.js"
+  "/fake-messenger/precache-manifest.d03c5c2660853ddbdcaf163ddc65c3e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fake-messenger/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
